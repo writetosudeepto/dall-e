6958946c8cff48bae09e4efce11b01971ec2d65b
writetosudeepto <writetosudeepto@gmail.com>
2023-02-05 02:50:18 +0530

    prompt handlechange fix

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -106,6 +106,7 @@ const CreatePost = () => {
             value={form.prompt}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
+            handleChange={handleChange}
           />
           <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
             {
